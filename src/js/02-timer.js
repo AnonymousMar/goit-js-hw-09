c4e194be310785a9caf72595908618ae7c090850
AnonymousMar <119819802+AnonymousMar@users.noreply.github.com>
2023-06-04 09:27:43 +0200

    .

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,7 @@
 import flatpickr from 'flatpickr'
 import 'flatpickr/dist/flatpickr.min.css'
-import Notiflix from 'notiflix'
-import 'notiflix/dist/notiflix-3.2.2.min.css'
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
     input: document.querySelector('#datetime-picker'),
