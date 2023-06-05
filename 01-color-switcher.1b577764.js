a07f11bb90c45ab98dccca63ba245f63a3db4647
AnonymousMar <119819802+AnonymousMar@users.noreply.github.com>
2023-06-05 06:13:38 +0000

    Deploying to gh-pages from @ AnonymousMar/goit-js-hw-09@335ec544203304870e2440e386f0b281e7b83fa6 🚀

---
+++ 01-color-switcher.1b577764.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");let a;function o(){const e=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;t.style.backgroundColor=e}d.disabled=!0,e.addEventListener("click",(()=>{a=setInterval(o,1e3),e.disabled=!0,d.disabled=!1})),d.addEventListener("click",(()=>{clearInterval(a),e.disabled=!1,d.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.1b577764.js.map
